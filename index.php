bec558c7df9ae0c361e4313ac90e29685b6886a1
Deoo0 <lawrencebersales8@gmail.com>
2025-02-12 21:35:10 +0800

    Initial Code for Navigation

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="javascript/main.js"></script>
+    <link rel="stylesheet" href="design/css/style.css">
+    <title>HOME</title>
+</head>
+<body>
+    <?php
+    error_reporting(1);
+    session_start();
+    include('nav.php');
+
+    
+    
+    
+    ?>
+    
+</body>
+</html>
