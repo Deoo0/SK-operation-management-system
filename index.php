ed35387825dddd6fbfdc5df3679d7cf41d760d46
Lawrence Deo Bersales <lawrencebersales8@gmail.com>
2025-02-12 10:16:38 +0800

    Added PHP Files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!-- 
+include('nav.php');
+    nav();
+
+    switch($_REQUEST['nav']){
+        case 'home':
+            include('home.php');
+            break;
+        case 'product':
+            include('product.php');
+            break;
+        case 'order':
+            include('order.php');
+            break;
+        case 'user':
+            include('user.php');
+            break;
+        case 'logout':
+            include('logout.php');
+            break;
+            
+        default:
+        include('home.php');
+        break;
+    } 
+-->
