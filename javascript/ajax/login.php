8de4d3090d1d65180031637a40bae51ddedcf322
Lawrence Deo Bersales <lawrencebersales8@gmail.com>
2025-02-12 10:24:25 +0800

    Added Code in login.php file

---
+++ javascript/ajax/login.php
@@ -0,0 +1,19 @@
+<div id="login-container">
+    <div id="login-form">
+        <form action="POST" id="loginForm">
+            <div id="login-items">
+                <label for="">
+                    <h1>
+                        <strong>
+                            LOGIN
+                        </strong>
+                    </h1>
+                </label>
+
+                <input type="username" id="username" placeholder="Enter Username">
+                <input type="password" id="password" placeholder="Enter Password">
+                <input type="submit" id="loginButton" value="Login" onclick="return loginBtn()">
+            </div>
+        </form>
+    </div>
+</div>
